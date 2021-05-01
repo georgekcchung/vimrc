0ed2ec47c643859c2c062888332e66a3254c18cc
George Chung <kcchung@home.kcchung.com>
2021-05-01 21:27:50 +0800

    fine tune install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt install cmake openjdk-11-jdk-headless vim python3 pip3 npm nodejs golang build-essential python3-pip ripgrep agrep ctags curl python3-venv -y
+sudo apt install openjdk-11-jdk-headless vim python3 python3-pip python3-venv golang build-essential cmake autoconf automake gdb git libffi-dev zlib1g-dev libssl-dev ripgrep agrep ctags curl -y
 cp vimrc ~/.vimrc
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-sudo pip3 install black
+pip3 install black
 vim -E -s -u ~/.vimrc +PlugInstall +qall
 cd ~/.vim/plugged/YouCompleteMe 
 python3 install.py --all 
