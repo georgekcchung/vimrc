f067aa4dce0eb7312f917b196da6c33ec230b227
George Chung <george@kcchung.com>
2021-05-02 01:00:38 +0800

    add nodejs npm

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt install openjdk-11-jdk-headless vim python3 python3-pip python3-venv golang build-essential cmake autoconf automake gdb git libffi-dev zlib1g-dev libssl-dev ripgrep agrep ctags curl -y
+sudo apt install openjdk-11-jdk-headless vim python3 python3-pip python3-venv python3-dev golang nodejs npm build-essential cmake autoconf automake gdb git libffi-dev zlib1g-dev libssl-dev ripgrep agrep ctags curl -y
 cp vimrc ~/.vimrc
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 pip3 install black
